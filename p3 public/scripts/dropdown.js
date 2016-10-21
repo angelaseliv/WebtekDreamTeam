1ac8aef3d5045dc74f83556493c86a4205b2f82a
Håvard Hunshamar <Hvard Hunshamar>
2016-10-21 12:56:58 +0200

    wtf

--- p3 public/scripts/dropdown.js
+++
@@ -1,7 +0,0 @@
-function dropdown() {
-    document.getElementById("myDropdown").style.display="block";
-};
-
-function notDropdown() {
-    document.getElementById("myDropdown").style.display="none";
-}
