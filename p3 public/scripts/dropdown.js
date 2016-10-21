50534b3031c007f044be289a1eb2424efc6ac7bf
Håvard Hunshamar <Hvard Hunshamar>
2016-10-21 12:29:04 +0200

    ddsadsfad

---
+++ p3 public/scripts/dropdown.js
@@ -0,0 +1,7 @@
+function dropdown() {
+    document.getElementById("myDropdown").style.display="block";
+};
+
+function notDropdown() {
+    document.getElementById("myDropdown").style.display="none";
+}
