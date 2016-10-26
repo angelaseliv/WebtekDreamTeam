549565b253414fd221fdd30211a70bee1179d5c6
ingridlarssen <ingrid_al_96@hotmail.com>
2016-10-26 11:23:46 +0200

    heihei håvard

--- p3public/scripts/galleribilder.js
+++ p3public/scripts/galleribilder.js
@@ -1,19 +1,5 @@
-function showImage(imgName) {
-document.getElementById('largeImg').src = imgName;
-showLargeImagePanel();
-unselectAll();
-
-}
-function showLargeImagePanel() {
-document.getElementById('largeImgPanel').style.visibility = 'visible';
-
-}
-
-function unselectAll() {
-if(document.selection) document.selection.empty();
-if(window.getSelection) window.getSelection().removeAllRanges();
-
-}
-
-function hideMe(obj) {
-obj.style.visibility = 'hidden';}
+Array.prototype.forEach.call(document.querySelector("img"), function (elem) {
+    elem.addEventListener("click", function () {
+        elem.classList.toggle("bilde");
+    });
+});
