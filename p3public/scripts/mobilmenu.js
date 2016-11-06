a411488c78a5d513a51052c5bcde59c83eb29bb9
havardhuns <havardhuns@gmail.com>
2016-11-06 15:20:34 +0100

    mobilmeny

--- p3public/scripts/mobilmenu.js
+++ p3public/scripts/mobilmenu.js
@@ -4,8 +4,11 @@ var innhold = document.getElementById("mobilmenuinnhold")
 function mobil(){
   if (innhold.style.display === "inline") {
     document.getElementById("mobilmenuinnhold").style.display = "none";
+    document.getElementById("menyknapp").style.backgroundColor = "#A48F6B"
   }
   else {
     document.getElementById("mobilmenuinnhold").style.display = "inline";
+    document.getElementById("menyknapp").style.backgroundColor = "#C6AA77"
+
   }
 }
