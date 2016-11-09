a86ac7daa0a09352b1e686cc8ee401e862b9ddd8
Håvard Hunshamar <Hvard Hunshamar>
2016-11-09 16:42:23 +0100

    sdad

--- p3public/scripts/mobilmenu.js
+++ p3public/scripts/mobilmenu.js
@@ -7,19 +7,24 @@ function mobil(){
   if (innhold.style.display === "inline") {
     document.getElementById("mobilmenuinnhold").style.display = "none";
     document.getElementById("menyknapp").style.backgroundColor = "#A48F6B"
+    document.getElementById("mobilmenygalleri").style.display = "inline";
+    document.getElementById("mobilmenyinspirasjon").style.display = "inline";
   }
   //Menyen åpnes hvis den ikke allerede er åpen, og knappen får en lysere farge.S
   else {
     document.getElementById("mobilmenuinnhold").style.display = "inline";
+    document.getElementById("ikkevis1").style.display = "none";
+    document.getElementById("ikkevis2").style.display = "none";
     document.getElementById("menyknapp").style.backgroundColor = "#C6AA77"
 
   }
 }
- /*
+
 function mobilchange(id) {
-  //venstre = document.getElementById(id);
-  //hoyre1 = document.getElementById("drop1")
-  //hoyre2 = document.getElementsByClassName("drop2")
+  var venstre = document.getElementById(id);
+  document.getElementById("mobilmenygalleri").style.display = "none"
+  document.getElementById("pil").style.display = "none";
+  venstre.style.display = "inline";
+
   console.log(id)
 }
-*/
